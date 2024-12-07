630e44225644027c80adacb244e1262bd85a3bcf
fawas4093 <fawas4093@gmail.com>
2024-12-07 20:40:42 +0530

    api change cors 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: 'https://sample11-mern-fpip-djurc1ol4-fawas4093s-projects.vercel.app/',
+    origin: 'https://sample11-mern-5926.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow Authorization header
   };
